a78ea146456e8e34112ae97f337fe8f3d8da2a00
Aluno Senai <lpauluch@gmail.com>
2023-11-14 22:01:29 -0300

    commit adicional

---
+++ lib/pages/second_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatefulWidget {
+  const SecondPage({super.key});
+
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Segunda Página'),
+        centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.white,
+      ),
+    );
+  }
+}
